3b5d2b268d443449a643f6a5b53866ca8b708d60
rushibagul0909 <rushibagul0909@gmail.com>
2022-11-07 11:56:33 +0530

    files added

---
+++ src/main/java/com/WP/ProjectWorkingProfessionalsApplication.java
@@ -0,0 +1,13 @@
+package com.WP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkingProfessionalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkingProfessionalsApplication.class, args);
+	}
+
+}
